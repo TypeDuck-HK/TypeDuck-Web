9a14059dbfc6aff8d47854bc53ca2a1df2b8f4c0
graphemecluster <graphemecluster@gmail.com>
2024-05-27 09:47:46 +0800

    Don’t Self-host Chiron Fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,3 @@
-import "chiron-hei-hk-webfont/css/vf.css";
-import "chiron-sung-hk-webfont/css/vf.css";
 import { createRoot } from "react-dom/client";
 import "react-toastify/dist/ReactToastify.css";
 
